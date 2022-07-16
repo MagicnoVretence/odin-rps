b9f215472cfec81d5b3e7ebfd350d981013a2e31
Matej Kern <matej_kern@msn.com>
2022-07-16 14:08:37 +0200

    added rps code to script

---
+++ rps.js
@@ -0,0 +1,46 @@
+const OPTIONS = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+    return OPTIONS[Math.floor(Math.random() * 3)];
+}
+
+function getPlayerChoice() {
+    let choice = prompt('Choose from "Rock", "Paper" or "Scissors": ');
+    let corrected = choice.at(0).toUpperCase().concat(choice.slice(1).toLowerCase());
+    return corrected;
+}
+
+function playRound(computerSelection, playerSelection) {
+    let computerIndex = OPTIONS.indexOf(computerSelection);
+    let playerIndex = OPTIONS.indexOf(playerSelection);
+    if (computerIndex == 2 && playerIndex == 0) {
+        return `You Win! ${OPTIONS[playerIndex]} beats ${OPTIONS[computerIndex]}!`;
+    } else if (computerIndex != 2 && playerIndex > computerIndex) {
+        return `You Win! ${OPTIONS[playerIndex]} beats ${OPTIONS[computerIndex]}!`;
+    } else {
+        return `You Lose! ${OPTIONS[computerIndex]} beats ${OPTIONS[playerIndex]}!`;
+    }
+}
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        let computer = getComputerChoice();
+        let player = getPlayerChoice();
+        if (!OPTIONS.some(x => x == player)) {
+            console.log('Not a valid choice! Quitting!');
+            break;
+        }
+        let result = playRound(computer, player);
+        if (result.startsWith('Win!', 4)) {
+            playerWins++;
+        } else {
+            computerWins++;
+        }
+        console.log(result);
+    }
+    console.log(`Overall result= Player-${playerWins}:Computer-${computerWins}`)
+}
+
+game();
