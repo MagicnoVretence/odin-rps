0bc297852dfb1e23fe7bd6ebcf247fe4da4e0e60
Matej Kern <matej_kern@msn.com>
2022-07-20 02:20:21 +0200

    fixed bugs, removed tie counter

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
 const OPTIONS = ['Rock', 'Paper', 'Scissors'];
 let playerWins = 0;
 let computerWins = 0;
-let ties = 0;
 let gamesPlayed = 0;
 
 const glavniDiv = document.getElementById('main-container');
@@ -10,7 +9,7 @@ const ispis2 = document.getElementById('result-summary');
 
 function playRound(event) {
     let computerIndex = Math.floor(Math.random() * 3);
-    let playerIndex = OPTIONS.indexOf(event.target.textContent);
+    let playerIndex = OPTIONS.indexOf(event.target.innerText);
     let resultText = '';
     if (computerIndex == 2 && playerIndex == 0) {
         resultText = `You Win! ${OPTIONS[playerIndex]} beats ${OPTIONS[computerIndex]}!`;
@@ -24,8 +23,8 @@ function playRound(event) {
         resultText = `You Lose! ${OPTIONS[computerIndex]} beats ${OPTIONS[playerIndex]}!`;
         computerWins++;
     };
-    ispis.textContent = resultText;
-    ispis2.textContent = `Player: ${playerWins} | Computer: ${computerWins} | Games played: ${gamesPlayed}`;
+    ispis.innerText = resultText;
+    ispis2.innerText = `Player: ${playerWins} | Computer: ${computerWins} | Games played: ${gamesPlayed}`;
     if ((playerWins > 4) || (computerWins > 4)) {
         let winner = '';
         if (playerWins > computerWins) {
@@ -34,13 +33,14 @@ function playRound(event) {
             winner = 'Computer';
         }
         gamesPlayed++;
+        ispis2.innerText = `Player: ${playerWins} | Computer: ${computerWins} | Games played: ${gamesPlayed}`;
         showEnding(winner);
     };
 }
 
 function showEnding(winner) {
     while (glavniDiv.firstChild) {
-        glavniDiv.firstChild.removeEventListener('click');
+        glavniDiv.firstChild.removeEventListener('click', playRound);
         glavniDiv.removeChild(glavniDiv.firstChild);
     };
     glavniDiv.classList.replace('playing', 'ended');
@@ -52,35 +52,39 @@ function showEnding(winner) {
     let buttonReset = document.createElement('button');
     buttonReset.innerText = 'Play again';
     buttonReset.addEventListener('click', setupGame);
+    glavniDiv.appendChild(buttonReset);
 }
 
 function setupGame() {
     playerWins = 0;
     computerWins = 0;
-    ties = 0;
+    ispis.innerText = '';
+    ispis2.innerText = `Player: ${playerWins} | Computer: ${computerWins} | Games played: ${gamesPlayed}`;
 
     while (glavniDiv.firstChild) {
-        glavniDiv.firstChild.removeEventListener('click');
+        glavniDiv.firstChild.removeEventListener('click', setupGame);
         glavniDiv.removeChild(glavniDiv.firstChild);
     };
     glavniDiv.classList.replace('ended', 'playing');
 
     let buttonRock = document.createElement('button');
     buttonRock.setAttribute('id', 'buttonRock');
-    buttonRock.textContent = 'Rock';
-    buttonRock.setAttribute('class', 'buttons');
+    buttonRock.innerText = 'Rock';
+    buttonRock.classList.add('buttons');
     buttonRock.addEventListener('click', playRound)
     let buttonPaper = document.createElement('button');
     buttonPaper.setAttribute('id', 'buttonPaper');
-    buttonPaper.textContent = 'Paper';
-    buttonPaper.setAttribute('class', 'buttons');
+    buttonPaper.innerText = 'Paper';
+    buttonPaper.classList.add('buttons');
     buttonPaper.addEventListener('click', playRound)
     let buttonScissors = document.createElement('button');
     buttonScissors.setAttribute('id', 'buttonScissors');
-    buttonScissors.textContent = 'Scissors';
-    buttonScissors.setAttribute('class', 'buttons');
+    buttonScissors.innerText = 'Scissors';
+    buttonScissors.classList.add('buttons');
     buttonScissors.addEventListener('click', playRound)
     glavniDiv.appendChild(buttonRock);
     glavniDiv.appendChild(buttonPaper);
     glavniDiv.appendChild(buttonScissors);
 }
+
+setupGame();
